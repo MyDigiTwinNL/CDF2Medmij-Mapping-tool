76f7bdcc7869b46e496998e37e4d280ab5c42167
Hector Cadavid <hector.cadavid@gmail.com>
2023-06-14 10:18:59 +0200

    HDL cholesterol template, module and tests included.

---
+++ src/__tests__/hdlcholesterol.ts
@@ -0,0 +1,116 @@
+import { InputSingleton } from '../inputSingleton';
+import * as hdlcholesterolmf from '../lifelines/HDLCholesterol'
+import {testResultFlagsSNOMEDCodelist} from '../snomedCodeLists';
+import { MappingTarget, processInput } from '../mapper'
+
+type HDLCholesterolProperties = {
+  "assessment":string,
+  "isHDLBelowReferenceRange":boolean,
+  "resultFlags": object,
+  "hdlResults": number,
+  "collectedDateTime": string
+}
+
+
+test('HDL Cholesterol reports, below reference lower limit', () => {
+  
+  const input = {
+
+    "hdlchol_result_all_m_1":       {"1A":(hdlcholesterolmf.referenceRangeLowerLimit()-0.1),"2A":(hdlcholesterolmf.referenceRangeLowerLimit()-0.5)},
+    "DATE": { "1A": "5/1992", "1B": "5/1995", "1C": "5/1997", "2A": "5/2001", "3A": "5/2003", "3B": "5/2005" },
+  }  
+
+  InputSingleton.getInstance().setInput(input);
+  const results = hdlcholesterolmf.results()
+  expect(results.length).toBe(2);  
+  expect((results[0] as HDLCholesterolProperties).isHDLBelowReferenceRange).toBe(true)
+  expect((results[0] as HDLCholesterolProperties).resultFlags).toBe(testResultFlagsSNOMEDCodelist.below_reference_range);
+  expect((results[1] as HDLCholesterolProperties).isHDLBelowReferenceRange).toBe(true)
+  expect((results[1] as HDLCholesterolProperties).resultFlags).toBe(testResultFlagsSNOMEDCodelist.below_reference_range);
+  
+
+});
+
+
+test('HDL Cholesterol reports, mix of normal and above reference ranges', () => {
+  
+  const input = {
+
+    "hdlchol_result_all_m_1":       {"1A":(hdlcholesterolmf.referenceRangeLowerLimit()+0.1),"2A":(hdlcholesterolmf.referenceRangeLowerLimit()-0.1)},
+    "DATE": { "1A": "5/1992", "1B": "5/1995", "1C": "5/1997", "2A": "5/2001", "3A": "5/2003", "3B": "5/2005" },
+  }  
+
+  InputSingleton.getInstance().setInput(input);
+  const results = hdlcholesterolmf.results()
+  expect(results.length).toBe(2);  
+  expect((results[0] as HDLCholesterolProperties).isHDLBelowReferenceRange).toBe(false)
+  expect((results[0] as HDLCholesterolProperties).resultFlags).toStrictEqual({});
+  expect((results[1] as HDLCholesterolProperties).isHDLBelowReferenceRange).toBe(true)
+  expect((results[1] as HDLCholesterolProperties).resultFlags).toBe(testResultFlagsSNOMEDCodelist.below_reference_range);
+  
+
+});
+
+
+test('HDL cholesterol reports, within normal levels', () => {
+  
+  const input = {
+
+    "hdlchol_result_all_m_1":       {"1A":(hdlcholesterolmf.referenceRangeLowerLimit()+0.1),"2A":(hdlcholesterolmf.referenceRangeLowerLimit()+0.5)},
+    "DATE": { "1A": "5/1992", "1B": "5/1995", "1C": "5/1997", "2A": "5/2001", "3A": "5/2003", "3B": "5/2005" },
+  }  
+
+  InputSingleton.getInstance().setInput(input);
+  const results = hdlcholesterolmf.results()
+  expect(results.length).toBe(2);  
+  expect((results[0] as HDLCholesterolProperties).isHDLBelowReferenceRange).toBe(false)
+  expect((results[0] as HDLCholesterolProperties).resultFlags).toStrictEqual({})
+  expect((results[1] as HDLCholesterolProperties).isHDLBelowReferenceRange).toBe(false)
+  expect((results[1] as HDLCholesterolProperties).resultFlags).toStrictEqual({})
+    
+
+});
+
+
+/**
+ * This tests only checks the module and its related templates work together, with no errors.
+ * FHIR-specific validations are expected to be done using the HL7 tools
+ */
+test('HDLCholesterol resource generation ()', () => {
+
+  
+  const input = {
+    "hdlchol_result_all_m_1":       {"1A":(hdlcholesterolmf.referenceRangeLowerLimit()+0.1),"2A":(hdlcholesterolmf.referenceRangeLowerLimit()-0.1)},
+    "DATE": { "1A": "5/1992", "1B": "5/1995", "1C": "5/1997", "2A": "5/2001", "3A": "5/2003", "3B": "5/2005" },
+    "AGE": { "1A": "22" },
+    "PROJECT_PSEUDO_ID": { "1A": "520681571" },
+  }
+
+  let targets: MappingTarget[] = [
+    { "template": './zib-2017-mappings/HDLCholesterol_Diagnostic_Report.jsonata', "module": './lifelines/HDLCholesterol'},
+  ]
+  
+  processInput(input,targets).then((output:object[]) => {
+    expect(output.length).toBe(2);    
+  })
+
+  targets = [
+    { "template": './zib-2017-mappings/HDLCholesterol_Observation.jsonata', "module": './lifelines/HDLCholesterol'},
+  ]
+  
+  processInput(input,targets).then((output:object[]) => {
+    expect(output.length).toBe(2);    
+  })
+
+  targets = [
+    { "template": './zib-2017-mappings/HDLCholesterol_Specimen.jsonata', "module": './lifelines/HDLCholesterol'}
+  ]
+  
+  processInput(input,targets).then((output:object[]) => {
+    expect(output.length).toBe(2);    
+  })
+
+
+})
+
+      
--- src/__tests__/ldlcholesterol.ts
+++ src/__tests__/ldlcholesterol.ts
@@ -31,6 +31,27 @@ test('Cholesterol reports, above reference range', () => {
 
 });
 
+
+test('Cholesterol reports, mix of normal and above reference ranges', () => {
+  
+  const input = {
+
+    "ldlchol_result_all_m_1":  {"1A":(cholesterolmf.referenceRangeUpperLimit()-0.1).toString(),"2A":(cholesterolmf.referenceRangeUpperLimit()+0.1).toString()},
+    "DATE": { "1A": "5/1992", "1B": "5/1995", "1C": "5/1997", "2A": "5/2001", "3A": "5/2003", "3B": "5/2005" },
+  }  
+
+  InputSingleton.getInstance().setInput(input);
+  const results = cholesterolmf.results()
+  expect(results.length).toBe(2);  
+  expect((results[0] as LDLCholesterolProperties).isLDLAboveReferenceRange).toBe(false)
+  expect((results[0] as LDLCholesterolProperties).resultFlags).toStrictEqual({});
+  expect((results[1] as LDLCholesterolProperties).isLDLAboveReferenceRange).toBe(true)
+  expect((results[1] as LDLCholesterolProperties).resultFlags).toBe(testResultFlagsSNOMEDCodelist.above_reference_range);
+  
+
+});
+
+
 test('Cholesterol reports, within normal levels', () => {
   
   const input = {
