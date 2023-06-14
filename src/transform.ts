f734e8ac267105f859275b718bbfc02fb686e289
Hector Cadavid <hector.cadavid@gmail.com>
2023-06-14 10:34:53 +0200

    HDL cholesterol template, module and tests included.

--- src/transform.ts
+++ src/transform.ts
@@ -2,14 +2,19 @@ import fs from 'fs';
 
 import {MappingTarget,transform} from './mapper'
 
-const input = JSON.parse(fs.readFileSync('/Users/hcadavid/eScience/MyDigiTwin/JSONataHarmonization/input/lifelines-ma/input.json', 'utf8'));
+const input = JSON.parse(fs.readFileSync('./input/lifelines-ma/input.json', 'utf8'));
 
 const targets:MappingTarget[] = [
   { "template": '../zib-2017-mappings/Diabetes.jsonata', "module": './lifelines/Diabetes'},
   { "template": '../zib-2017-mappings/LDLCholesterol_Diagnostic_Report.jsonata', "module": './lifelines/LDLCholesterol'},
   { "template": '../zib-2017-mappings/LDLCholesterol_Observation.jsonata', "module": './lifelines/LDLCholesterol'},
   { "template": '../zib-2017-mappings/LDLCholesterol_Specimen.jsonata', "module": './lifelines/LDLCholesterol' },
-  { "template": '../zib-2017-mappings/Hypertension.jsonata', "module": './lifelines/Hypertension' }
+  { "template": '../zib-2017-mappings/Hypertension.jsonata', "module": './lifelines/Hypertension' },
+  { "template": '../zib-2017-mappings/HDLCholesterol_Diagnostic_Report.jsonata', "module": './lifelines/HDLCholesterol'},
+  { "template": '../zib-2017-mappings/HDLCholesterol_Observation.jsonata', "module": './lifelines/HDLCholesterol'},
+  { "template": '../zib-2017-mappings/HDLCholesterol_Specimen.jsonata', "module": './lifelines/HDLCholesterol' },
+  
+
 ]
 
 
