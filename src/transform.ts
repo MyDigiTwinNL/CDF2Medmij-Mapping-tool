69381f36df62f0b9933853253ec44b54edcec06a
Hector Cadavid <hector.cadavid@gmail.com>
2023-06-14 11:01:49 +0200

    main module as CLI

--- src/transform.ts
+++ src/transform.ts
@@ -2,7 +2,6 @@ import fs from 'fs';
 
 import {MappingTarget,transform} from './mapper'
 
-const input = JSON.parse(fs.readFileSync('./input/lifelines-ma/input.json', 'utf8'));
 
 const targets:MappingTarget[] = [
   { "template": '../zib-2017-mappings/Diabetes.jsonata', "module": './lifelines/Diabetes'},
@@ -12,14 +11,50 @@ const targets:MappingTarget[] = [
   { "template": '../zib-2017-mappings/Hypertension.jsonata', "module": './lifelines/Hypertension' },
   { "template": '../zib-2017-mappings/HDLCholesterol_Diagnostic_Report.jsonata', "module": './lifelines/HDLCholesterol'},
   { "template": '../zib-2017-mappings/HDLCholesterol_Observation.jsonata', "module": './lifelines/HDLCholesterol'},
-  { "template": '../zib-2017-mappings/HDLCholesterol_Specimen.jsonata', "module": './lifelines/HDLCholesterol' },
-  
+  { "template": '../zib-2017-mappings/HDLCholesterol_Specimen.jsonata', "module": './lifelines/HDLCholesterol' }
 
 ]
 
 
-transform(input,targets).then((output)=>{
-  console.info(JSON.stringify(output));
+const main = (inputPath:string) =>{
+  const input = JSON.parse(fs.readFileSync(inputPath, 'utf8'));
+    transform(input,targets).then((output)=>{
+      console.info(JSON.stringify(output));
+    }
+  )  
 }
-)
+
+
+// Check if the file exists
+const validateFileExistence = (fileName: string):boolean => {
+  try {
+    fs.accessSync(fileName);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+// Get command line arguments
+const args: string[] = process.argv.slice(2);
+
+// Extract the file name
+const fileName: string | undefined = args[0];
+
+if (fileName) {
+  if (validateFileExistence(fileName)) {
+    main(fileName);
+  } else {
+    console.log(`File '${fileName}' does not exist.`);
+  }
+} else {
+  console.log(`Expected input: node ${__filename.slice(__dirname.length + 1, -3)} <file-name>`);
+}
+
+
+
+
+
+
+
 
