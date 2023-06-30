1156ed8645c5a0afaf2bb24143101bff6123fe20
Hector Cadavid <hector.cadavid@gmail.com>
2023-06-30 15:24:08 +0200

    Validation script now using a single FHIR validation execution for all the generated samples

--- fhirvalidation/fhirvalidator.js
+++ fhirvalidation/fhirvalidator.js
@@ -6,10 +6,9 @@ const { execSync } = require('child_process');
 // Directory containing the files
 const directoryPath = './sampleinputs';
 
-// Generate a random folder name to store the output files
-const randomFolderName = Math.random().toString(36).substring(7);
-const outputFolder = fs.mkdtempSync(os.tmpdir() + '/') + randomFolderName;
-fs.mkdirSync(outputFolder);
+// Generate a random folder name to store the temporary input files and the JAR output
+const outputTempFolder = fs.mkdtempSync(path.join(os.tmpdir(), Math.random().toString(36).substring(7)));
+const logsTempFolder = fs.mkdtempSync(path.join(os.tmpdir(), Math.random().toString(36).substring(7)));
 
 // Compile TypeScript code
 execSync('tsc');
@@ -23,32 +22,31 @@ if (!process.env.fhirvpath) {
 // Get the list of files in the directory
 const files = fs.readdirSync(directoryPath);
 
-// Execute the TypeScript code for each file
-files.forEach((file) => {
-  // Get the full path of the file
+// Create temporary input files
+const tempInputFiles = files.map((file) => {
   const filePath = path.join(directoryPath, file);
-
-  // Execute the TypeScript code and redirect the output to a random file in the output folder
   const randomFileName = Math.random().toString(36).substring(7) + '.json';
-  const outputFile = path.join(outputFolder, randomFileName);
-  const tsCommand = `node ../dist/transform ${filePath} > ${outputFile}`;
-  console.info(`Running ${tsCommand}`)
+  const tempInputFile = path.join(outputTempFolder, randomFileName);
+  const tsCommand = `node ../dist/transform  ${filePath} > ${tempInputFile}`;
+
+  console.info(`Executing ${tsCommand}`)
+
   execSync(tsCommand);
+  return tempInputFile;
 });
 
-// Execute the JAR command for each output file
-const outputFiles = fs.readdirSync(outputFolder);
-outputFiles.forEach((outputFile) => {
-  // Get the full path of the output file
-  const outputFilePath = path.join(outputFolder, outputFile);
-
-  // Execute the JAR command
-  const jarCommand = `java -jar ${process.env.fhirvpath}/validator_cli.jar ${outputFilePath} -version 3.0.2 -ig nictiz.fhir.nl.stu3.zib2017#2.2.8 -sct nl -output-style compact`;
-  try {
-    execSync(jarCommand);
-    console.log(`JAR command for ${outputFile} exited with code 0`);
-  } catch (error) {
-    console.error(`JAR command for ${outputFile} execution failed with non-zero exit code`);
+// Execute the JAR command with the path to temporary input files
+const jarOutputFile = path.join(logsTempFolder, 'output.log.txt');
+const jarCommand = `java -jar ${process.env.fhirvpath}/validator_cli.jar ${outputTempFolder} -version 3.0.2 -ig nictiz.fhir.nl.stu3.zib2017#2.2.8 -sct nl -output-style compact > ${jarOutputFile}`;
+try {
+  console.log(`Executing ${jarCommand}`)
+  execSync(jarCommand);
+  console.log(`Command exited with code 0. Log file path:${jarOutputFile}`);
+} catch (error) {
+  console.error('JAR command execution failed with non-zero exit code');
+  if (error.stderr) {
+    console.error('Error output:');
+    console.error(error.stderr);
   }
-});
+}
 
