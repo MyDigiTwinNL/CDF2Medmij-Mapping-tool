3c676c7a975ae0343d90998518d676d9245e3ffd
Hector Cadavid <hector.cadavid@gmail.com>
2023-07-05 09:27:58 +0200

    more command line arguments included

---
+++ README.md
@@ -0,0 +1,46 @@
+# Cohort-study to FHIR/MedMij mapping tool
+
+
+
+## Table of Contents
+
+- [Cohort-study to FHIR/MedMij mapping tool](#cohort-study-to-fhirmedmij-mapping-tool)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Features](#features)
+  - [Contributing](#contributing)
+  - [License](#license)
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/username/project.git`
+2. Navigate to the project directory: `cd project`
+3. Install the dependencies: `npm install`
+
+## Usage
+
+1. Build the project: `npm run build`
+2. Start the application: `npm start`
+
+## Features
+
+- Feature 1: Description of feature 1.
+- Feature 2: Description of feature 2.
+- Feature 3: Description of feature 3.
+
+## Contributing
+
+Contributions are welcome! Here's how you can contribute to the project:
+
+1. Fork the repository.
+2. Create a new branch: `git checkout -b feature/your-feature-name`
+3. Make your changes and commit them: `git commit -m 'Add some feature'`
+4. Push the branch to your forked repository: `git push origin feature/your-feature-name`
+5. Submit a pull request detailing your changes.
+
+Please make sure to update tests as appropriate and ensure that your code follows the project's coding standards.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- src/transform.ts
+++ src/transform.ts
@@ -18,36 +18,123 @@ const targets:MappingTarget[] = [
 
 ]
 
+//To resolve all relative paths from the 'dist' folder.
+const folderPath = path.resolve(__dirname);
+process.chdir(folderPath);
 
-const main = (inputPath:string) =>{
 
-  //To resolve all relative paths from the 'dist' folder.
-  const folderPath = path.resolve(__dirname);
-  process.chdir(folderPath);
-  
-  const input = JSON.parse(fs.readFileSync(inputPath, 'utf8'));
+const inputFileToStdout = (filePath:string) =>{
+  const input = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     transform(input,targets).then((output)=>{
       console.info(JSON.stringify(output));
     }
   )  
 }
 
+const inputFileToFolder = (filePath:string,outputFolder:string) =>{
+  const input = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    transform(input,targets).then((output)=>{
+
+      const fileName = path.basename(filePath);
+      const fileExtension = path.extname(filePath);
+      const fileNameWithoutExtension = fileName.replace(fileExtension, '');
+      const fhirFileName = `${fileNameWithoutExtension}-fhir${fileExtension}`;
+      const outputFilePath = path.join(outputFolder, fhirFileName);
+    
+      fs.writeFileSync(outputFilePath, JSON.stringify(output));
+
+    }
+  )  
+}
+
+
 
 // Check if a file exists
-const validateFileExistence = (fileName: string):boolean => {
+const validateFileExistence = (filePath: string):boolean => {
   try {
-    fs.accessSync(fileName);
+    fs.accessSync(filePath);
     return true;
   } catch (error) {
     return false;
   }
 }
 
+const validateFolderExistence = (folderPath: string): boolean => {
+  try {
+    const stats = fs.statSync(folderPath);
+    return stats.isDirectory();
+  } catch {
+    return false;
+  }
+}
+
+
+function printCommandLineArguments(): void {
+  console.log('Program parameters details:');
+  console.log('Process all the files in a folder (output folder is mandatory):');
+  console.log(`node ${__filename.slice(__dirname.length + 1, -3)} <input folder path> -o <output folder path>`);
+  console.log('Process a single file and generate a file with the output in a given folder:');
+  console.log(`node ${__filename.slice(__dirname.length + 1, -3)} <input file path> -o <output folder path>`);
+  console.log('Process a single file and print the output on STDOUT:');
+  console.log(`node ${__filename.slice(__dirname.length + 1, -3)} <input file path>`);
+}
+
+function processArguments(args: string[]): void {
+  if (args.length === 0) {
+    printCommandLineArguments()
+    return;
+  }
+
+  let folderPath: string | null = null;
+  let filePath: string | null = null;
+  let outputFolder: string | null = null;
+
+  if (args.length === 1) {
+    const arg = args[0];
+    if (validateFolderExistence(arg)) {
+      console.error('Error: a folder path was given as an input, but the output folder is missing (-o option followed by the output folder)');
+      return;
+    } else if (validateFileExistence(arg)) {
+      inputFileToStdout(arg);
+    } else {
+      console.error(`Error: the path or folder given as an input does not exist: '${arg}'`);
+      return;
+    }
+  } 
+  else if (args.length === 3) {
+    const arg1 = args[0];
+    const arg2 = args[1];
+    const arg3 = args[2];
+
+    if (arg2 === '-o') {
+      if (validateFolderExistence(arg1)) {
+        folderPath = arg1;
+        outputFolder = arg3;
+      } else if (validateFileExistence(arg1)) {
+        filePath = arg1;
+        outputFolder = arg3;
+      } else {
+        console.error(`Error: Invalid path '${arg1}'`);
+        return;
+      }
+    } else {
+      console.error('Error: Invalid command');
+      printCommandLineArguments();
+      return;
+    }
+  } else {
+    console.error('Error: Invalid command');
+    printCommandLineArguments()
+    return;
+  }
+}
+
 // Get command line arguments
 const args: string[] = process.argv.slice(2);
+processArguments(args);
 
 // Extract the file name
-const fileName: string | undefined = args[0];
+/*const fileName: string | undefined = args[0];
 
 if (fileName) {
   const absFilePath=path.resolve(fileName)
@@ -58,7 +145,7 @@ if (fileName) {
   }
 } else {
   console.log(`Expected input: node ${__filename.slice(__dirname.length + 1, -3)} <file-name>`);
-}
+}*/
 
 
 
