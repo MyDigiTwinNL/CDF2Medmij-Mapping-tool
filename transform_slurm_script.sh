fd56cecb8857658692c5bb052753fa85259e1bbe
Hector Cadavid <hector.cadavid@gmail.com>
2023-07-29 17:52:17 +0000

    build clean on slurm script

--- transform_slurm_script.sh
+++ transform_slurm_script.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=CSV2CDF
 #SBATCH --output=cdf2fhir.out
 #SBATCH --error=cdf2fhir.err
-#SBATCH --time=02:59:00
+#SBATCH --time=03:59:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4gb
+#SBATCH --mem=2gb
 #SBATCH --nodes=1
 #SBATCH --open-mode=append
 #SBATCH --export=NONE
@@ -14,6 +14,6 @@
 module load nodejs/16.15.1-GCCcore-11.3.0
 module list
 
-node_modules/typescript/bin/tsc
+node_modules/typescript/bin/tsc --build --clean
 #npm run transform --  ../data_samples_tiny/ -o /home/umcg-hcadavid/temporal-data/pheno_lifelines_fhir
 npm run transform -- /home/umcg-hcadavid/temporal-data/pheno_lifelines_csd_out -o /home/umcg-hcadavid/temporal-data/pheno_lifelines_fhir
