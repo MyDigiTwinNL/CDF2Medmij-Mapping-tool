1bcafe409177658d0d045f6561b50d9db7576106
Hector Cadavid <hector.cadavid@gmail.com>
2023-07-29 17:09:24 +0000

    slurm batch script

---
+++ transform_slurm_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=CSV2CDF
+#SBATCH --output=cdf2fhir.out
+#SBATCH --error=cdf2fhir.err
+#SBATCH --time=02:59:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4gb
+#SBATCH --nodes=1
+#SBATCH --open-mode=append
+#SBATCH --export=NONE
+#SBATCH --get-user-env=L60
+
+
+module load nodejs/16.15.1-GCCcore-11.3.0
+module list
+
+node_modules/typescript/bin/tsc
+#npm run transform --  ../data_samples_tiny/ -o /home/umcg-hcadavid/temporal-data/pheno_lifelines_fhir
+npm run transform -- /home/umcg-hcadavid/temporal-data/pheno_lifelines_csd_out -o /home/umcg-hcadavid/temporal-data/pheno_lifelines_fhir
