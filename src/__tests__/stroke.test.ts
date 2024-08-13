5d5a2a5e3bbc3ca9c559895fee15d5fb7e3bc28e
Hector Cadavid <hector.cadavid@gmail.com>
2024-08-13 15:43:00 +0200

    startage removed from test cases where the stroked is reported after the baseline assessment (for consistency with the expected inputs)

--- src/__tests__/stroke.test.ts
+++ src/__tests__/stroke.test.ts
@@ -27,7 +27,7 @@ test('stroke, when reported positive in 1A', () => {
 test('stroke, when reported in 2A', () => {
 
   const input = {
-    "stroke_startage_adu_q_1":{ "1a": "12" },
+    "stroke_startage_adu_q_1":{ "1a": "" },
     "stroke_presence_adu_q_1": { "1a": "2" },
     "stroke_followup_adu_q_1":{"1b":"2","1c":"2","2a":"1","3a":"2","3b":"2"},    
     "date": {"1a":"1992-5","1b":"1995-5","1c":"1997-5","2a":"2001-5","3a":"2003-5","3b":"2005-5"},
@@ -46,7 +46,7 @@ test('stroke, when reported in 2A', () => {
 test('stroke, when reported right after baseline (1B)', () => {
 
   const input = {
-    "stroke_startage_adu_q_1":{ "1a": "12" },
+    "stroke_startage_adu_q_1":{ "1a": "" },
     "stroke_presence_adu_q_1": { "1a": "2" },
     "stroke_followup_adu_q_1":{"1b":"1","1c":"2","2a":"2","3a":"2","3b":"2"},    
     "date": {"1a":"1992-5","1b":"1994-5","1c":"1997-5","2a":"2001-5","3a":"2003-5","3b":"2005-5"},
@@ -65,7 +65,7 @@ test('stroke, when reported right after baseline (1B)', () => {
 test('stroke, when reported in 2A, after skipping one assessment', () => {
 
   const input = {
-    "stroke_startage_adu_q_1":{ "1a": "12" },
+    "stroke_startage_adu_q_1":{ "1a": "" },
     "stroke_presence_adu_q_1": { "1a": "2" },
     "stroke_followup_adu_q_1":{"1b":"2","1c":undefined,"2a":"1","3a":"2","3b":"2"},    
     "date": {"1a":"1992-5","1b":"1995-5","1c":undefined,"2a":"2001-5","3a":"2003-5","3b":"2005-5"},
@@ -84,7 +84,7 @@ test('stroke, when reported in 2A, after skipping one assessment', () => {
 test('stroke, when reported in 2A, after skipping multiple assessments', () => {
 
   const input = {
-    "stroke_startage_adu_q_1":{ "1a": "12" },
+    "stroke_startage_adu_q_1":{ "1a": "" },
     "stroke_presence_adu_q_1": { "1a": "2" },
     "stroke_followup_adu_q_1":{"1b":undefined,"1c":undefined,"2a":"1","3a":"2","3b":"2"},    
     "date": {"1a":"1992-5","1b":undefined,"1c":undefined,"2a":"2002-5","3a":"2003-5","3b":"2005-5"},
@@ -104,7 +104,7 @@ test('stroke, when reported in 2A, after skipping multiple assessments', () => {
 test('stroke, when no reported', () => {
 
   const input = {
-    "stroke_startage_adu_q_1":{ "1a": "12" },
+    "stroke_startage_adu_q_1":{ "1a": "" },
     "stroke_presence_adu_q_1": { "1a": "2" },
     "stroke_followup_adu_q_1":{"2a":"2","3a":"2","3b":"2"},
     "date": {"1a":"1992-5","1b":"1995-5","1c":"1997-5","2a":"2001-5","3a":"2003-5","3b":"2005-5"},
@@ -125,7 +125,7 @@ test('Stroke resource generation when not reported', () => {
 
   const input = {
     "project_pseudo_id": {"1a":"520681571"},
-    "stroke_startage_adu_q_1":{ "1a": "12" },
+    "stroke_startage_adu_q_1":{ "1a": "" },
     "stroke_presence_adu_q_1": { "1a": "2" },
     "stroke_followup_adu_q_1":{"2a":"2","3a":"2","3b":"2"},
     "date": {"1a":"1992-5","1b":"1995-5","1c":"1997-5","2a":"2001-5","3a":"2003-5","3b":"2005-5"},
